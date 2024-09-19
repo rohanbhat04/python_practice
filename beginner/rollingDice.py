8b0d2fbb4db617d3d0a0bf1166441e0c3257fbe6
rohan bhat <140295911+rohanbhat04@users.noreply.github.com>
2024-09-19 21:37:11 +0530

    Create rollingDice.py
    created first beginner projects of a rolling dice using random package

---
+++ beginner/rollingDice.py
@@ -0,0 +1,20 @@
+import random
+
+def roll_dice():
+    list_num = []
+    num = random.randint(1,6)
+    list_num.append(num)
+    num = random.randint(1,6)
+    list_num.append(num)
+    return list_num
+
+
+while True:
+    choice = input("Roll a dice ? (Y/N) : ")
+    if(choice == "Y" or choice == "y"):
+        num_on_dice = roll_dice()
+        print(num_on_dice)
+
+    else:
+        print("Thank You for playing with me :)")
+        quit()
